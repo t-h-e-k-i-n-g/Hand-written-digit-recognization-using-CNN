5b65ea50f6d24dd2aef736e5951672ffbd8e46e1
t-h-e-k-i-n-g <65018558+t-h-e-k-i-n-g@users.noreply.github.com>
2020-05-08 16:59:03 +0530

    Add files via upload

---
+++ digit.py
@@ -0,0 +1,42 @@
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+#load data
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+
+#flatten 28*28 images to a 784 vector for each image
+num_pixels=x_train.shape[1]*x_train.shape[2]
+x_train=x_train.reshape((x_train.shape[0],num_pixels)).astype('float32')
+x_test=x_test.reshape((x_test.shape[0],num_pixels)).astype('float32')
+
+#normalise inputs fropm 0-255 to 0-1
+x_train=x_train/255
+x_test=x_test/255
+
+#one hot encode outputs
+y_train= np_utils.to_categorical(y_train)
+y_test= np_utils.to_categorical(y_test)
+num_classes=y_test.shape[1]
+
+#model
+def baseline_model():
+    model=Sequential()
+    model.add(Dense(num_pixels,input_dim=num_pixels,init='normal',activation='relu'))
+    model.add(Dense(num_classes,init='normal',activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# build the model
+model = baseline_model()
+
+# Fit the model
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
+
+# Final evaluation of the model
+scores = model.evaluate(x_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+
+
+# -*- coding: utf-8 -*-
+
